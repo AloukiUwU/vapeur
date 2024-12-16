bc6009e25174e95bf8317684b57a6798d88b8cf7
AloukiUwU <akilotxx@gmail.com>
2024-12-16 12:03:07 +0100

    routes created and files added

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const { PrismaClient } = require("@prisma/client");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
+const prisma = new PrismaClient();
+const PORT = 3000;
+
+// On définit un middleware pour parser les données des requêtes entrantes.
+// Cela permet de récupérer les données envoyées via des formulaires et les rendre disponibles dans req.body.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
+
+app.get("/", async (req, res) => {
+    // on passe seulement le nom du fichier .hbs sans l'extention.
+    // Le chemin est relatif au dossier `views`.
+
+    // const fewGames;
+
+    res.render("accueil", { 
+        //fewGames, 
+    });
+});
+
+app.get("/editors", async (req, res) => {
+    res.render("editors");
+});
+
+app.get("/genres", async (req, res) => {
+    res.render("genres");
+});
+
+app.get("/games", async (req, res) => {
+    res.render("games/games");
+});
+
+app.get("/games/:id", async (req, res) => {
+    res.render("games/detailsGame", {
+        //req.params.id,
+    });
+});
+
+app.get("/newGame", async (req, res) => {
+    res.render("games/newGame");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
