97708fc24da9eda119b3b94d9bcd01cc95f2183d
AloukiUwU <akilotxx@gmail.com>
2024-12-16 17:43:21 +0100

    css c bo

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ const PORT = 3000;
 // Cela permet de récupérer les données envoyées via des formulaires et les rendre disponibles dans req.body.
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Configuration de Handlebars pour Express
 app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
 app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
 hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
+
+app.use(express.static('css'));
 
 
 // Helper pour vérifier l'égalité
